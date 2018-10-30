45d71e33b06d9110c3dc98ea5d7e5d03c71325d8
Burgerking10 <43968522+Burgerking10@users.noreply.github.com>
2018-10-30 11:21:01 -0700

    Factorials

---
+++ Factorialtwo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Factorialtwo
+{
+    public static void main (String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Type in a small integer:");
+        int n = s.nextInt
